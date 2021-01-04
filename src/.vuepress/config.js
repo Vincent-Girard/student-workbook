4fce195578fb1f55f5f3af7050a4e6dd2f96323e
Vincent-Girard <66797358+Vincent-Girard@users.noreply.github.com>
2021-01-04 15:19:45 -0700

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { config } = require("vuepress-theme-hope");
 
 
-const GITHUB_USERNAME = 'REPLACE_ME' // TODO 
+const GITHUB_USERNAME = 'Vincent-Girard' // TODO 
 
 
 module.exports = config({
